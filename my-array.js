64228faeae71c12a41df7c83f9dcf7a2d06a1a50
Paifon Chatchaya <chatchaya.pf@gmail.com>
2026-07-02 13:49:27 +0700

    add array and object

---
+++ my-array.js
@@ -0,0 +1,3 @@
+let food = ["Paizza", "Malatang", "Pad Thai", "Burger", "Sushi", "Medovik"];
+
+console.log(food[3]);
---
+++ my-object.js
@@ -0,0 +1,47 @@
+// "Paizza", "Malatang", "Pad Thai", "Burger", Sushi", "Medovik"
+
+let foodDatabase = [
+    {
+        id: 1,
+        name: "Pizza", 
+        cuisine: "Italian",
+        origin: "Intaly",
+        vegetarian: false,
+    },
+    {
+        id: 2, 
+        name: "Malatang", 
+        cuisine: "Italian",
+        origin: "Intaly",
+        vegetarian: false,
+    },
+    {
+        id: 3, 
+        name: "Pad Thai",
+        cuisine: "Italian",
+        origin: "Intaly",
+        vegetarian: false,
+    },
+    {
+        id: 4,
+        name: "Burger",
+        cuisine: "Italian",
+        origin: "Intaly",
+        vegetarian: false,
+    },
+    {
+        id: 5, 
+        name: "Sushi",
+        cuisine: "Italian",
+        origin: "Intaly",
+        vegetarian: false,
+    },
+    {
+        id: 6, 
+        name: "Medovik",
+        cuisine: "Italian",
+        origin: "Intaly",
+        vegetarian: false,
+    }
+];
+
